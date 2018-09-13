c55ae048b6414d757256331c96458b4a10c25cc8
art <art.huang@ehsn.com.tw>
2018-09-13 10:25:36 +0800

    first red

---
+++ LeetCode_412_BDD_Tests/Scenraio/Solution.feature
@@ -0,0 +1,11 @@
+﻿#language: zh-TW
+功能: FizzBuzzLibTests
+
+場景: 呼叫GetResult方法並傳入2，結果應為長度2的字串集合
+	假設 陣列長度輸入 2 
+	當   呼叫GetResult方法
+	那麼 結果為
+	| value |
+	| 1     |
+	| 2     |
+
---
+++ LeetCode_412_BDD_Tests/Steps/FizzBuzzLibTestsSteps.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ExpectedObjects;
+using LeetCode_412_BDD;
+using LeetCode_412_BDD_Tests.Extension;
+using TechTalk.SpecFlow;
+
+namespace LeetCode_412_BDD_Tests.Steps
+{
+    [Binding]
+    public class FizzBuzzLibTestsSteps
+    {
+        [Given(@"陣列長度輸入 (.*)")]
+        public void 假設陣列長度輸入(int length)
+        {
+            ScenarioContext.Current.Set(length, "length");
+        }
+
+        [When(@"呼叫GetResult方法")]
+        public void 當呼叫GetResult方法()
+        {
+            var length = ScenarioContext.Current.Get<int>("length");
+            var sut = new Solution();
+            var actual = sut.FizzBuzz(length);
+            ScenarioContext.Current.Set(actual, "actual");
+        }
+
+        [Then(@"結果為")]
+        public void 那麼結果為(Table table)
+        {
+            var expected = table.AsStrings("value");
+            var actual = ScenarioContext.Current.Get<IList<string>>("actual");
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
